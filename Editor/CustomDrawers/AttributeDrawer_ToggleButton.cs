f7331ce3b3d7fe5b89855ada3a1fc149a7455075
Marmik <144893751+Marmik-Emp37@users.noreply.github.com>
2024-12-02 18:50:16 +0530

    Update: Improved feedback in ToggleButtonAttribute drawer.
    • Boolean fields using the ToggleButtonAttribute will dynamically append the current state ("On" or "Off") to the label text.

--- Editor/CustomDrawers/AttributeDrawer_ToggleButton.cs
+++ Editor/CustomDrawers/AttributeDrawer_ToggleButton.cs
@@ -14,6 +14,7 @@ namespace Emp37.Utility.Editor
                         EditorGUI.HelpBox(position, $"Use {nameof(ToggleButtonAttribute)} on 'Boolean' field type.", UnityEditor.MessageType.Error);
                         return;
                   }
+                  label.text += " : " + (property.boolValue ? "On" : "Off");
                   property.boolValue = GUI.Toggle(position, property.boolValue, label, GUI.skin.button);
             }
             public override float GetPropertyHeight(SerializedProperty property, GUIContent label) => (attribute as ToggleButtonAttribute).Height;
